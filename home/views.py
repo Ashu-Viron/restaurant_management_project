d0388f90a7f7d5e751641c7f07cfa94c80746ac2
Ashu <97926707+Ashu-Viron@users.noreply.github.com>
2025-08-14 23:30:19 +0530

    31298

--- home/views.py
+++ home/views.py
@@ -4,3 +4,16 @@ from .models import Restaurant
 def homepage(request):
     restaurant = Restaurant.objects.first()
     return render(request, 'home.html', {'restaurant_name': restaurant.name if restaurant else 'My Restaurant'})
+
+
+from django.conf import settings
+from django.shortcuts import render
+
+def home(request):
+    restaurant_name = "My Restaurant"  
+    phone_number = settings.RESTAURANT_PHONE_NUMBER
+    return render(request, "home.html", {
+            "restaurant_name": restaurant_name,
+            "phone_number": phone_number
+    })
+
